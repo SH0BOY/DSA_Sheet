d84cf5bb002be4a293ec83b8a0c7a2c4c41c1cec
shoboy <sshoboyyy@gmail.com>
2026-06-11 22:34:49 +0530

    Optised problem 1047

--- Problems/1047.java
+++ Problems/1047.java
@@ -1,23 +1,17 @@
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character> stk = new Stack<>();
-
-        for(char i :s.toCharArray()){
-            if(stk.isEmpty()){
-                stk.push(i);
-            }
-            else if(stk.peek()==i){
-                stk.pop();
-            }
-            else{
-                stk.push(i);
-            }
-        }
         StringBuilder sb = new StringBuilder();
 
-        for(Character i : stk){
-            sb.append(i);
+        for(char i : s.toCharArray()){
+            int len =sb.length();
+            if(len > 0 && sb.charAt(len-1)==i){
+                sb.deleteCharAt(len-1);
+            }
+            else{
+                sb.append(i);
+            }
         }
+
         return sb.toString();
     }
 }
